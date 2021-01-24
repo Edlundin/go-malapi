fa9043f86b45fbbf2e73548c2cbdc9f6ffc92698
Edvard LUNDIN <edvard.lundin@pm.me>
2021-01-24 05:20:49 +0100

    feat(maloauth2): web server that listen for MAL OAuth2 callback

--- internal/maloauth2/maloauth2.go
+++ internal/maloauth2/maloauth2.go
@@ -1,8 +1,10 @@
 package maloauth2
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 
 	"github.com/google/uuid"
@@ -49,3 +51,49 @@ func GenerateMalOAuth2AuthorizationURL(clientID string, redirectURI string) {
 	log.Println(authorizationURL)
 }
 
+func ListenForMalOAuth2Callback(port uint) <-chan map[string]string {
+	const malOAuth2CallbackRoute string = "/maloauth2callback"
+	const codeParamName string = "code"
+	const stateParamName string = "state"
+	const callbackHTTPMethod string = http.MethodGet
+
+	httpServerHandler := http.NewServeMux()
+	httpServer := &http.Server{Addr: fmt.Sprintf("0.0.0.0:%d", port), Handler: httpServerHandler}
+	queryParamChan := make(chan map[string]string)
+
+	httpServerHandler.HandleFunc(malOAuth2CallbackRoute, func(writer http.ResponseWriter, request *http.Request) {
+		if request.URL.Path == malOAuth2CallbackRoute {
+			if request.Method == callbackHTTPMethod {
+				code, state := request.URL.Query().Get(codeParamName), request.URL.Query().Get(stateParamName)
+
+				if len(code) > 0 && len(state) > 0 {
+					writer.WriteHeader(200)
+					queryParamChan <- map[string]string{codeParamName: code, stateParamName: state}
+				} else {
+					log.Println(fmt.Sprintf("request argument missing (len(%s): %d, len(%s): %d", codeParamName, len(code), stateParamName, len(state))) //TODO: hide in production environment (development log) //TODO: replace by a proper logger
+				}
+			} else {
+				http.Error(writer, "", http.StatusMethodNotAllowed)
+				log.Println(fmt.Sprintf("wanted method: %q, got: %q", callbackHTTPMethod, request.Method)) //TODO: hide in production environment (development log) //TODO: replace by a proper logger
+			}
+		} else {
+			http.Error(writer, "", http.StatusNotFound)
+			log.Println(fmt.Sprintf("received request on %s instead of %s", request.URL.Path, malOAuth2CallbackRoute)) //TODO: hide in production environment (development log) //TODO: replace by a proper logger
+		}
+	})
+
+	go func() { // goroutine to stop the web server when MAL have sent the code and state back
+		select {
+		case <-queryParamChan:
+			httpServer.Shutdown(context.Background())
+		}
+	}()
+
+	go func() { //goroutine to serve a web server in a non-blocking manner
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalln(fmt.Sprintf("An error occurred with the web server on %s: %s", httpServer.Addr, err.Error()))
+		}
+	}()
+
+	return queryParamChan
+}
