f0cc6a9a728ba7e2273b55fa7334acf5aa97e84f
Edvard LUNDIN <edvard.lundin@pm.me>
2021-01-31 03:48:18 +0100

    refactor(maloauth2): generate OAuth2 authorization URL wihtout global variables

--- internal/maloauth2/maloauth2.go
+++ internal/maloauth2/maloauth2.go
@@ -11,35 +11,42 @@ import (
 	pkce "github.com/nirasan/go-oauth-pkce-code-verifier"
 )
 
-var codeVerifier string
-var codeChallenge string
+//OAuth2AuthorizationData holds the data needed to obtain an access token and a refresh token from MAL's API.
+type OAuth2AuthorizationData struct {
+	pkceCodeChallenge string
+	pkceCodeVerifier  string
+	authorizationURL  string
+	state             string
+}
 
-func GenerateMalOAuth2AuthorizationURL(clientID string, redirectURI string) {
+//GenerateMalOAuth2AuthorizationURL generates an authorization URL for MAL's API.
+func GenerateMalOAuth2AuthorizationURL(clientID string, redirectURI string) (OAuth2AuthorizationData, error) {
 	const malOauth2AuthorizationEndPoint string = "https://myanimelist.net/v1/oauth2/authorize"
 	const responseType string = "code"
 	const codeChallengeMethod string = "plain"
 
+	authorizationData := OAuth2AuthorizationData{}
 	pkceGenerator, err := pkce.CreateCodeVerifier()
 
-	if err != nil {
-		log.Fatalln(fmt.Sprintf("error with pkce generator: %s", err.Error()))
+	if err == nil {
+		return authorizationData, fmt.Errorf("pkce generation: %s", err.Error())
 	}
 
-	codeVerifier = pkceGenerator.String()
-	codeChallenge = pkceGenerator.CodeChallengePlain()
-	state := uuid.New().String()
+	authorizationData.pkceCodeVerifier = pkceGenerator.String()
+	authorizationData.pkceCodeChallenge = pkceGenerator.CodeChallengePlain()
+	authorizationData.state = uuid.New().String()
 
 	authorizationURL, err := url.Parse(malOauth2AuthorizationEndPoint)
 
 	if err != nil {
-		log.Fatalln(fmt.Sprintf("An error occurred while parsing %s as an URL: %s", malOauth2AuthorizationEndPoint, err.Error())) //TODO: replace by a proper logger
+		return authorizationData, fmt.Errorf("url generation: %s", err.Error())
 	}
 
 	query := authorizationURL.Query()
 	query.Add("response_type", responseType)
 	query.Add("client_id", clientID)
-	query.Add("code_challenge", codeChallenge)
-	query.Add("state", state)
+	query.Add("code_challenge", authorizationData.pkceCodeChallenge)
+	query.Add("state", authorizationData.state)
 	query.Add("code_challenge_method", codeChallengeMethod)
 
 	if len(redirectURI) > 0 {
@@ -47,18 +54,22 @@ func GenerateMalOAuth2AuthorizationURL(clientID string, redirectURI string) {
 	}
 
 	authorizationURL.RawQuery = query.Encode()
+	authorizationData.authorizationURL = authorizationURL.String()
 
-	log.Println(authorizationURL)
+	return authorizationData, nil
 }
 
-func ListenForMalOAuth2Callback(port uint) <-chan map[string]string {
+//ListenForMalOAuth2Callback creates a web server listening on the URI "http://host:port" (passing host and port by arguments).
+//Upon receiving a MAL API callback, its arguments ("code" and "state") are parsed and returned by a map-typed channel.
+//After receiving these arguments, the web server will shut itself down.
+func ListenForMalOAuth2Callback(host string, listeningPort uint) <-chan map[string]string {
 	const malOAuth2CallbackRoute string = "/maloauth2callback"
 	const codeParamName string = "code"
 	const stateParamName string = "state"
 	const callbackHTTPMethod string = http.MethodGet
 
 	httpServerHandler := http.NewServeMux()
-	httpServer := &http.Server{Addr: fmt.Sprintf("0.0.0.0:%d", port), Handler: httpServerHandler}
+	httpServer := &http.Server{Addr: fmt.Sprintf("%s:%d", host, listeningPort), Handler: httpServerHandler}
 	queryParamChan := make(chan map[string]string)
 
 	httpServerHandler.HandleFunc(malOAuth2CallbackRoute, func(writer http.ResponseWriter, request *http.Request) {
@@ -82,14 +93,14 @@ func ListenForMalOAuth2Callback(port uint) <-chan map[string]string {
 		}
 	})
 
-	go func() { // goroutine to stop the web server when MAL have sent the code and state back
+	go func() { // goroutine stopping the web server when MAL have sent back the code and state
 		select {
 		case <-queryParamChan:
 			httpServer.Shutdown(context.Background())
 		}
 	}()
 
-	go func() { //goroutine to serve a web server in a non-blocking manner
+	go func() { //goroutine serving a web server in a non-blocking manner
 		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalln(fmt.Sprintf("An error occurred with the web server on %s: %s", httpServer.Addr, err.Error()))
 		}
