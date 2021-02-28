a4bdf9e5c86b14ddfe1e4b33410f45c9033bbe1f
Edvard LUNDIN <edvard.lundin@pm.me>
2021-02-28 22:21:24 +0100

    docs(anime): documente RankingType enum

--- pkg/mal/anime/ranking.go
+++ pkg/mal/anime/ranking.go
@@ -1,29 +1,39 @@
 package anime
 
+//RankingType represents the type of anime ranking.
 type RankingType int
 
 const (
-	RankTopAnime RankingType = iota + 1
-	RankTopAiring
-	RankTopUpcoming
-	RankTopTV
-	RankTopOVA
-	RankTopMovie
-	RankTopSpecial
-	RankByPopularity
-	RankFavorited
+	//RankingTopAnime represents the top animes.
+	RankingTopAnime RankingType = iota + 1
+	//RankingTopAiring represents the top airines animes.
+	RankingTopAiring
+	//RankingTopUpcoming represents the top upcoming animes.
+	RankingTopUpcoming
+	//RankingTopTV represents the top TV animes.
+	RankingTopTV
+	//RankingTopOVA represents the top OVA animes.
+	RankingTopOVA
+	//RankingTopMovie represents the top movie animes.
+	RankingTopMovie
+	//RankingTopSpecial represents the top special animes.
+	RankingTopSpecial
+	//RankingByPopularity represents the top animes by popularity.
+	RankingByPopularity
+	//RankingFavorited represents the top favorited by user animes.
+	RankingFavorited
 )
 
 var rankStrDict = map[RankingType]string{
-	RankTopAnime:     "all",
-	RankTopAiring:    "airing",
-	RankTopUpcoming:  "upcoming",
-	RankTopTV:        "tv",
-	RankTopOVA:       "ova",
-	RankTopMovie:     "movie",
-	RankTopSpecial:   "special",
-	RankByPopularity: "bypopularity",
-	RankFavorited:    "favorite",
+	RankingTopAnime:     "all",
+	RankingTopAiring:    "airing",
+	RankingTopUpcoming:  "upcoming",
+	RankingTopTV:        "tv",
+	RankingTopOVA:       "ova",
+	RankingTopMovie:     "movie",
+	RankingTopSpecial:   "special",
+	RankingByPopularity: "bypopularity",
+	RankingFavorited:    "favorite",
 }
 
 func (rankingType RankingType) String() string {
