6444d350d3bc19d971e8ec953ce973e3ae6baa5a
Edvard LUNDIN <edvard.lundin@pm.me>
2021-03-09 22:01:04 +0100

    refactoring: move ParseError from /pkg/anime to /internal

--- pkg/mal/anime/parse.go
+++ pkg/mal/anime/parse.go
@@ -1,30 +1,9 @@
 package anime
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/Edlundin/go-malapi/pkg/mal"
 )
 
-//ParseError parses MAL's JSON error message.
-func ParseError(responseBody []byte) (mal.Error, error) {
-	var malError mal.Error
+func parseSearchAnimeResponse(responseBody []byte) ([]AnimeObject, mal.Page, error) {
 
-	err := json.Unmarshal(responseBody, &malError)
-
-	if err != nil {
-		return malError, fmt.Errorf("demarshalling response body: %s", err.Error())
-	}
-
-	if len(malError.Error) == 0 {
-		return malError, fmt.Errorf("value for key %q is empty", "error")
-	}
-
-	if len(malError.Message) == 0 {
-		return malError, fmt.Errorf("value for key %q is empty", "message")
-	}
-
-	return malError, nil
 }
-
