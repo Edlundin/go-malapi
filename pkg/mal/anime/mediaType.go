d35e5fdbab85071f646796c71b5047def23f6a5c
Edvard LUNDIN <edvard.lundin@pm.me>
2021-05-17 19:55:12 +0200

    feat(anime): implements MediaType enum

---
+++ pkg/mal/anime/mediaType.go
@@ -0,0 +1,72 @@
+package anime
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+//MediaType represents a media type.
+type MediaType int
+
+const (
+	//MediaTypeTV represents the TV media.
+	MediaTypeTV MediaType = iota + 1
+	//MediaTypeOVA represents the OVA media.
+	MediaTypeOVA
+	//MediaTypeMovie represents the movie media.
+	MediaTypeMovie
+	//MediaTypeSpecial represents the special media.
+	MediaTypeSpecial
+	//MediaTypeONA represents the ONA media.
+	MediaTypeONA
+	//MediaTypeMusic represents the music media.
+	MediaTypeMusic
+	//MediaTypeUnknown represents an unknown media type.
+	MediaTypeUnknown
+)
+
+var mediaTypeStrDict = map[MediaType]string{
+	MediaTypeTV:      "tv",
+	MediaTypeOVA:     "ova",
+	MediaTypeMovie:   "movie",
+	MediaTypeSpecial: "special",
+	MediaTypeONA:     "ona",
+	MediaTypeMusic:   "music",
+	MediaTypeUnknown: "unknown",
+}
+
+func (m *MediaType) UnmarshalJSON(b []byte) error {
+	var listStatusStr string
+
+	if err := json.Unmarshal(b, &listStatusStr); err != nil {
+		return err
+	}
+
+	found := false
+
+	for key, v := range mediaTypeStrDict {
+		if v == listStatusStr {
+			*m = key
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("%q is not a media type", listStatusStr)
+	}
+
+	return nil
+}
+
+//String returns the string representation of an enum value.
+//If the value is not valid (e.g. undefined enum value), this functions returns "undefined".
+func (m MediaType) String() string {
+	mediaTypeStr := "undefined"
+
+	if str, ok := mediaTypeStrDict[m]; ok {
+		mediaTypeStr = str
+	}
+
+	return mediaTypeStr
+}
---
+++ pkg/mal/anime/mediaType_test.go
@@ -0,0 +1,51 @@
+package anime
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func Test_MediaType_UnmarshalJSON(t *testing.T) {
+	var mediaType MediaType
+
+	if err := json.Unmarshal([]byte(`""`), &mediaType); err == nil {
+		t.Error("failed to pass empty string: no error returned")
+	}
+
+	if err := json.Unmarshal([]byte(`" "`), &mediaType); err == nil {
+		t.Error("failed to pass space string: no error returned")
+	}
+
+	if err := json.Unmarshal([]byte(`"test"`), &mediaType); err == nil {
+		t.Errorf("failed to pass malformed media type (%q): no error returned", "test")
+	}
+
+	for mediaTypeEnum, mediaTypeStr := range mediaTypeStrDict {
+		if err := json.Unmarshal([]byte(fmt.Sprintf("%q", mediaTypeStr)), &mediaType); err != nil {
+			t.Errorf("failed to pass well formed media type: %s", err.Error())
+		} else if mediaType != mediaTypeEnum {
+			t.Errorf("failed to pass well formed media type: %q(%d) != %q(%d)", mediaType.String(), mediaType, mediaTypeStr, mediaTypeEnum)
+		}
+	}
+}
+
+func Test_MediaType_String(t *testing.T) {
+	for mediaTypeEnum, mediaTypeStr := range mediaTypeStrDict {
+		if mediaTypeEnum.String() != mediaTypeStr {
+			t.Errorf("failed to pass existing media type: %q != %q for enum value %d", mediaTypeEnum.String(), mediaTypeStr, mediaTypeEnum)
+		}
+	}
+
+	var mediaType MediaType
+
+	if mediaType.String() != "undefined" {
+		t.Error(`failed to pass un-initialized media type: the returned string should be "undefined"`)
+	}
+
+	mediaType = MediaTypeUnknown + 5
+
+	if mediaType.String() != "undefined" {
+		t.Error(`failed to pass undefined media type: the returned string should be "undefined"`)
+	}
+}
